a51f2f02a0053770717950661235cb4a5093d237
jvejar1 <jvejar1@miuandes.cl>
2019-10-12 17:18:00 -0500

    first commit

---
+++ Assets/Player/PlayerBehaviour.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+public class PlayerBehaviour : NetworkBehaviour
+{
+
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (isLocalPlayer) {
+
+            float moveHorizontal = Input.GetAxis("Horizontal");
+            float moveVertical = Input.GetAxis("Vertical");
+            CmdMove(moveHorizontal, moveVertical);
+
+        }
+
+     
+    }
+
+    [Command]
+    void CmdMove(float horizontal, float vertical) {
+        
+        Vector3 movement = new Vector3(horizontal, 0.0f, vertical);
+        transform.Translate(movement);
+
+    }
+}
