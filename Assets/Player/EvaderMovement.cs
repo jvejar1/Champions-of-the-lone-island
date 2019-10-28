1a10b328a285f5f00d48f16b06b473989901ea86
jvejar1 <jvejar1@miuandes.cl>
2019-10-28 12:53:24 -0600

    player: evader movement

---
+++ Assets/Player/EvaderMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class EvaderMovement : NetworkBehaviour
+{
+    // Start is called before the first frame update
+
+    float evasorMovementVelocity = 10;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isLocalPlayer)
+        {
+
+            return;
+        }
+        //check input
+
+        bool shiftPressed = Input.GetKeyDown(KeyCode.LeftShift);
+        
+        if (shiftPressed)
+        {
+            Debug.Log("Left shift pressed!");
+            CmdEvaderMovement();
+        }
+
+    }
+
+    [Command]
+    void CmdEvaderMovement()
+    {
+        Vector3 movement = new Vector3(10.0f, 0f,0f);
+        transform.Translate(movement);
+    }
+}
